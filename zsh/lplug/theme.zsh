475d42f6a2cf82d0c64525077cbb8401be4a9bbd
liyechen <gliyechen@hotmail.com>
2020-05-11 23:44:27 +0800

    update

--- zsh/lplug/theme.zsh
+++ zsh/lplug/theme.zsh
@@ -77,7 +77,7 @@ precmd() {
 
 # Define prompts
 #
-PROMPT="%(?.%F{magenta}.%F{red})❯%f " # Display a red prompt char on failure
+PROMPT="%(?.%F{magenta}.%F{red})_%f " # Display a red prompt char on failure
 RPROMPT="%F{8}${SSH_TTY:+%n@%m}%f"    # Display username if connected via SSH
 
 # ------------------------------------------------------------------------------
